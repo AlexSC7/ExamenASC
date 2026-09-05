6278e9441d4e357451205853a27864fa32c9747f
Alejandro Salazar <alexzsphone@gmail.com>
2026-09-04 20:22:22 -0600

    test: Creación de test de clases de servicio

---
+++ src/test/java/com/exam/asc/service/ClienteServiceTest.java
@@ -0,0 +1,83 @@
+package com.exam.asc.service;
+
+import com.exam.asc.application.port.in.SincronizarPedidosClienteUseCase;
+import com.exam.asc.application.port.out.ClienteRepositoryPort;
+import com.exam.asc.application.service.ClienteService;
+import com.exam.asc.domain.model.Cliente;
+import com.exam.asc.domain.model.Item;
+import com.exam.asc.domain.model.Pedido;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class ClienteServiceTest {
+
+    ClienteRepositoryPort repoFalso = Mockito.mock(ClienteRepositoryPort.class);
+    SincronizarPedidosClienteUseCase sincronizarPedidos = Mockito.mock(SincronizarPedidosClienteUseCase.class);
+    ClienteService service = new ClienteService(repoFalso, sincronizarPedidos);
+
+    @Test
+    void debeRegistrarUnCliente() {
+
+        Cliente cliente = new Cliente(
+                "id",
+                "Juan",
+                "Perez",
+                "Lopez",
+                "correo@correo",
+                "calle",
+                List.of(new Pedido(
+                        "id",
+                        "id",
+                        "online",
+                        "10/10/2026",
+                        "Liv",
+                        List.of(new Item(
+                                "id",
+                                1,
+                                "pantalon"
+                        ))
+                )));
+        when(repoFalso.guardar(cliente)).thenReturn(cliente);
+
+        Cliente resultado = service.crearCliente(cliente);
+
+        assertEquals("Juan", resultado.nombre());
+    }
+
+    @Test
+    void debeConsultaUnClientePorId() {
+
+        Cliente cliente = new Cliente(
+                "id",
+                "Juan",
+                "Perez",
+                "Lopez",
+                "correo@correo",
+                "calle",
+                List.of(new Pedido(
+                        "id",
+                        "id",
+                        "online",
+                        "10/10/2026",
+                        "Liv",
+                        List.of(new Item(
+                                "id",
+                                1,
+                                "pantalon"
+                        ))
+                )));
+
+        when(repoFalso.buscarPorId("id")).thenReturn(Optional.of(cliente));
+
+        Optional<Cliente> resultado = service.consultarCliente("id");
+
+        assertEquals("Juan", resultado.<Object>map(Cliente::nombre).orElse(null));
+    }
+
+}
---
+++ src/test/java/com/exam/asc/service/SincronizarPedidosClienteServiceTest.java
@@ -0,0 +1,87 @@
+package com.exam.asc.service;
+
+import com.exam.asc.application.port.out.ClienteRepositoryPort;
+import com.exam.asc.application.port.out.PedidosApiPort;
+import com.exam.asc.application.service.SincronizarPedidosClienteService;
+import com.exam.asc.domain.model.Cliente;
+import com.exam.asc.domain.model.Item;
+import com.exam.asc.domain.model.Pedido;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SincronizarPedidosClienteServiceTest {
+    PedidosApiPort pedidosApiMock = Mockito.mock(PedidosApiPort.class);
+    ClienteRepositoryPort clienteRepoMock = Mockito.mock(ClienteRepositoryPort.class);
+
+    @Test
+    void debeSincronizarSoloLosPedidosDelUsuario() {
+
+
+        when(pedidosApiMock.obtenerPedidos())
+                .thenReturn(List.of(new Pedido(
+                        "",
+                        "id",
+                        "online",
+                        "10/10/2026",
+                        "Liv",
+                        List.of(new Item(
+                                "id",
+                                1,
+                                "pantalon"
+                        )))));
+
+        when(clienteRepoMock.buscarPorId("user-123"))
+                .thenReturn(Optional.of(clienteDePrueba()));
+
+        SincronizarPedidosClienteService service =
+                new SincronizarPedidosClienteService(pedidosApiMock, clienteRepoMock);
+
+        service.sincronizar("user-123");
+
+        verify(clienteRepoMock).actualizar(argThat(cliente ->
+                cliente.ordenes().contains(new Pedido(
+                        "",
+                        "id",
+                        "online",
+                        "10/10/2026",
+                        "Liv",
+                        List.of(new Item(
+                                "id",
+                                1,
+                                "pantalon"
+                        ))))
+                        && cliente.ordenes().size() == 1
+        ));
+
+
+    }
+
+    Cliente clienteDePrueba() {
+        return new Cliente(
+                "user-123",
+                "Juan",
+                "Perez",
+                "Lopez",
+                "correo@correo",
+                "calle",
+                List.of(new Pedido(
+                        "id",
+                        "id",
+                        "online",
+                        "10/10/2026",
+                        "Liv",
+                        List.of(new Item(
+                                "id",
+                                1,
+                                "pantalon"
+                        ))
+                )));
+    }
+}
